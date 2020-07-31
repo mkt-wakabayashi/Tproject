e37e7cbb83491f3c1b83a4e216a9eaf43245c54f
Takehiro14 <takehiro.thanks@gmail.com>
2020-07-31 17:56:50 +0900

    部分テンプレート

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <header>
-    <h1>ameeeda</h1>
-  </header>
-  <main></main>
-  <footer></footer>
-</body>
-</html>
+@extends('template')
+
+@section('content')
+  <p>hello</p>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet"  href="{{asset('assets/css/index.css')}}">
+  <title>ameeeda 勤怠管理</title>
+</head>
+<body>
+  <div class="wrapper">
+    <header>
+      <h1>ameeeda</h1>
+    </header>
+    @yield('content')
+    <footer>
+      <small>Copyright 2020 ameeeda,Inc.</small>
+    </footer>
+  </div>
+</body>
+</html>
