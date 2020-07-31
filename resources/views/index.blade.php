809c4c9a09780ab7c64194421ab7550cb78ed92d
Takehiro14 <takehiro.thanks@gmail.com>
2020-07-31 19:07:26 +0900

    update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,19 @@
 @extends('template')
 
 @section('content')
-  <p>hello</p>
+  <main>
+    <nav>
+      <ul>
+        <li>account</li>
+        <li><a href="#">name</a></li>
+        <li><a href="#">application</a></li>
+        <li><a href="#">list</a></li>
+        <li><a href="#">logout</a></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class="time-field"></div>
+      <div class="time-field"></div>
+    </div>
+  </main>
 @endsection
