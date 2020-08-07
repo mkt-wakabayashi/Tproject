323c18d8555e47a88b60086c2e55e8eac0e10e79
Takehiro14 <takehiro.thanks@gmail.com>
2020-08-07 13:02:02 +0900

    create HomeController.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('content')
+
+<a href="/index">勤怠ページ(ログイン)</a>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
+
+Route::get('/', 'HomeController@home');
+
+Route::get('/index', 'HomeController@index');
